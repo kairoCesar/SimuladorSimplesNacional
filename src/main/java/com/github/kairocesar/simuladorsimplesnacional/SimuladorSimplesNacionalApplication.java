b8772336ba5d9b291f3cbf5bf67f83800f252513
kairoCesar <kairo8121@gmail.com>
2024-03-07 21:14:35 -0300

    initial commit

---
+++ src/main/java/com/github/kairocesar/simuladorsimplesnacional/SimuladorSimplesNacionalApplication.java
@@ -0,0 +1,13 @@
+package com.github.kairocesar.simuladorsimplesnacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorSimplesNacionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimuladorSimplesNacionalApplication.class, args);
+    }
+
+}
